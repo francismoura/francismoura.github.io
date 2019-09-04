b7da66cb3d6386acf1ffb0fc7c57edd9927fd59b
francismoura <francis.gulla@gmail.com>
2019-09-04 18:14:43 -0300

    1.0

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
+(function() {
+    const btn_to_top = document.getElementById("return-to-top");
+    // When the user clicks on the button, scroll to the top of the document
+    btn_to_top.addEventListener("click", function(event) {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera  
+    });
 
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("return-to-top").style.display = "block";
-  } else {
-    document.getElementById("return-to-top").style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+    // When the user scrolls down 20px from the top of the document, show the button
+    document.addEventListener("scroll", function() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            btn_to_top.style.display = "block";
+        } else {
+            btn_to_top.style.display = "none";
+        }
+    });
+})(document);
