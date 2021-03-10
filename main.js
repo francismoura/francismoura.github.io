6883717bb80b3ce53219998ac01915e144b6daf0
Francis Moura <francis.gulla@gmail.com>
2021-03-10 17:05:41 -0300

    Atualização de conteúdo e melhorias no laytou

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 (function() {
+
     const btn_to_top = document.getElementById("return-to-top");
     // When the user clicks on the button, scroll to the top of the document
     btn_to_top.addEventListener("click", function(event) {
@@ -8,7 +9,7 @@
 
     // When the user scrolls down 20px from the top of the document, show the button
     document.addEventListener("scroll", function() {
-        if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+        if (document.body.scrollTop > 335 || document.documentElement.scrollTop > 335) {
             btn_to_top.style.display = "block";
         } else {
             btn_to_top.style.display = "none";
