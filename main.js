d38ff2aa880d87dc925bb682c4f3edd0d62badda
lemaFrancis <francis.lemaf@gmail.com>
2021-03-10 00:59:21 -0300

    Melhoria no layout e atualiação da imagem de perfil

--- main.js
+++ main.js
@@ -3,12 +3,12 @@
     // When the user clicks on the button, scroll to the top of the document
     btn_to_top.addEventListener("click", function(event) {
         document.body.scrollTop = 0; // For Safari
-        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera  
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     });
 
     // When the user scrolls down 20px from the top of the document, show the button
     document.addEventListener("scroll", function() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
             btn_to_top.style.display = "block";
         } else {
             btn_to_top.style.display = "none";
